4b67f2e1c93759a7d15cf5e6202df6944fb75ae2
lanceethanong <lance_ethan_s_ong@dlsu.edu.ph>
2026-04-05 18:43:22 +0800

    added a create if not exists line in sql

--- sql/lab_res_db (updated).sql
+++ sql/lab_res_db (updated).sql
@@ -17,6 +17,10 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+
+CREATE DATABASE IF NOT EXISTS `lab_res_db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `lab_res_db`;
+
 --
 -- Database: `lab_res_db`
 --
